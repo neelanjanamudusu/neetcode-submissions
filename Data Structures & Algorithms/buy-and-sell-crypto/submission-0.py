017186a7ae6e1d1ed1648674ef71eab7a392715a
MUDUSU NEELANJANA <37610561+neelanjanamudusu@users.noreply.github.com>
2026-05-23 19:15:00 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        profit = 0
+        for price in prices:
+            if price < buy:
+                buy = price
+            if price - buy > profit:
+                profit = price - buy
+        return profit
